8dc65d954715f5631e7ade9a427d9468a2f1d62a
Stewest <weststew@gmail.com>
2023-05-02 21:10:25 +1200

    improve arrow function

--- gatsby/src/components/container.js
+++ gatsby/src/components/container.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from "react"
 
-export default ({ children, className }) => (
+const Container = ({ children, className }) => (
   <div className={className}>{children}</div>
-);
+)
+
+export default Container
