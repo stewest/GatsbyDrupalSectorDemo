da155012ac6ebf279751dd21b38c21a18290b8c9
Stewest <weststew@gmail.com>
2023-05-09 20:56:18 +1200

    remove gatsby-source-drupal-menu-links

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -4,8 +4,9 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-console.log(process.env.DRUPAL_BASE_URL)
-console.log(process.env.DRUPAL_BASIC_AUTH_USERNAME)
+console.log("baseUrl: ", process.env.DRUPAL_BASE_URL)
+console.log("basic auth User: ", process.env.DRUPAL_BASIC_AUTH_USERNAME)
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby with Drupal Sector Example`,
@@ -17,7 +18,7 @@ module.exports = {
         typePrefix: "Drupal",
         apiBase: `jsonapi`,
         baseUrl: process.env.DRUPAL_BASE_URL,
-        concurrentFileRequests: 2,
+        concurrentFileRequests: 10,
         fastBuilds: true,
         basicAuth: {
           username: process.env.DRUPAL_BASIC_AUTH_USERNAME,
@@ -36,14 +37,14 @@ module.exports = {
         update: process.env.GATSBY_UPDATE_SCHEMA_SNAPSHOT,
       },
     },
-    {
-      resolve: `gatsby-source-drupal-menu-links`,
-      options: {
-        typePrefix: "Drupal",
-        baseUrl: process.env.DRUPAL_BASE_URL,
-        apiBase: `jsonapi`, // optional, defaults to `jsonapi`
-        menus: ["main", "main-menu", "account"], // Which menus to fetch, there are the menu IDs.
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-drupal-menu-links`,
+    //   options: {
+    //     typePrefix: "Drupal",
+    //     baseUrl: process.env.DRUPAL_BASE_URL,
+    //     apiBase: `jsonapi`, // optional, defaults to `jsonapi`
+    //     menus: ["main", "main-menu", "account"], // Which menus to fetch, there are the menu IDs.
+    //   },
+    // },
   ],
 }
